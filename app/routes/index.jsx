1a676bfcaa3cc1c62604050f642c883b3ccef6ba
Pixelshot <pixelshot8@gmail.com>
2022-09-20 21:00:04 +0800

    General and nested routes are done. Moving on to loaders. 25:52

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,5 +1,13 @@
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <h1>Welcome to Remix!</h1>
+      <p>
+        Remix is a full stack web framework by the creators of React Router.
+        This is a simple blog app from Traversy Media Remix crash course.
+      </p>
+    </div>
+  );
 }
 
 export default Home;
---
+++ app/routes/posts/new.jsx
@@ -0,0 +1,9 @@
+function NewPost() {
+  return (
+    <div>
+      <h1>This is the New Post section</h1>
+    </div>
+  );
+}
+
+export default NewPost;
